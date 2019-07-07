df6da6aa658a0113f3ec8b55bcec9e1c6e4e124b
Shchava <yarosshc@gmail.com>
2019-07-08 00:42:47 +0300

    Initial commit

---
+++ src/main/java/ua/training/hospital/WebConfig.java
@@ -0,0 +1,8 @@
+package ua.training.hospital;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+}
---
+++ src/main/java/ua/training/hospital/controller/StartController.java
@@ -0,0 +1,12 @@
+package ua.training.hospital.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StartController {
+    @RequestMapping("/")
+    public String index(){
+        return "index.jsp";
+    }
+}
