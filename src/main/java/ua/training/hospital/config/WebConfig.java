de2067efe670d3356776d634852419d5c223fc52
Shchava <yarosshc@gmail.com>
2019-07-26 00:13:46 +0300

    change session locale resolver to cookie locale resolver

--- src/main/java/ua/training/hospital/config/WebConfig.java
+++ src/main/java/ua/training/hospital/config/WebConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
@@ -20,7 +20,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        return new SessionLocaleResolver();
+        CookieLocaleResolver r = new CookieLocaleResolver();
+        r.setCookieName("lang");
+        return r;
     }
 
     @Bean
