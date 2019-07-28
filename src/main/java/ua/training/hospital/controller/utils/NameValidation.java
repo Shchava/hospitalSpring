1b97d224e6951817f74383fd97ab06210edcbd31
Shchava <yarosshc@gmail.com>
2019-07-28 22:53:08 +0300

    add name validator

---
+++ src/main/java/ua/training/hospital/controller/utils/NameValidation.java
@@ -0,0 +1,18 @@
+package ua.training.hospital.controller.utils;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE, FIELD,ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = NameValidator.class)
+public @interface NameValidation {
+    String message() default "name dont matches regex";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
