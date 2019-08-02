c84e3d6ca5569ae2b2d07c1943b44c7b720af50f
Shchava <yarosshc@gmail.com>
2019-08-02 20:38:16 +0300

    fix findMedicineByDiagnosisId query

--- src/main/java/ua/training/hospital/repository/MedicineRepository.java
+++ src/main/java/ua/training/hospital/repository/MedicineRepository.java
@@ -5,11 +5,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import ua.training.hospital.entity.Diagnosis;
 import ua.training.hospital.entity.Medicine;
 
 public interface MedicineRepository extends PagingAndSortingRepository<Medicine, Long> {
-    @Query("SELECT m FROM Medicine m " +
-            "WHERE m.diagnosis = :diagnosisId")
+    @Query("FROM Medicine m " +
+            "WHERE m.diagnosis.idDiagnosis = :diagnosisId")
     Page<Medicine> findMedicineByDiagnosisId(Pageable page, @Param("diagnosisId") Long diagnosisId);
 }
