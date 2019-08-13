39aaf3557f450c968d5974f50fa1d30ebb9898d1
Shchava <yarosshc@gmail.com>
2019-08-13 17:14:07 +0300

    add logging for error controller

--- src/main/java/ua/training/hospital/controller/ErrorController.java
+++ src/main/java/ua/training/hospital/controller/ErrorController.java
@@ -1,5 +1,7 @@
 package ua.training.hospital.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +13,23 @@ import java.util.concurrent.locks.ReadWriteLock;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    private static final Logger logger = LogManager.getLogger(ErrorController.class);
+
+
     @RequestMapping(value = "/error", method = RequestMethod.GET)
     public String handleError(HttpServletRequest request) {
+        logger.debug(" /error requested");
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
 
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                logger.debug(" http status 404, returning pageNotFound.jsp page");
                 return "pageNotFound";
             }
         }
+        logger.debug("returning error.jsp page");
         return getErrorPath();
     }
 
